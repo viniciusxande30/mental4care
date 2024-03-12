c4b559af401477f74878fe110e2d237489588f4e
viniciusxande30 <vinicius.xande30@gmail.com>
2024-03-11 23:31:12 -0300

    Commit Email

--- app/Http/Controllers/EmailController.php
+++ app/Http/Controllers/EmailController.php
@@ -10,13 +10,45 @@ class EmailController extends Controller
 {
     public function enviarEmail(Request $request)
 {
-    // Valide os dados do formulário se necessário
-    // Selecionar os campos do formulário que você deseja incluir no e-mail
-    $dadosFormulario = $request->only(['nome', 'email', 'empresa','qtd','telefone','mensagem']);
+    // // Valide os dados do formulário se necessário
+    // // Selecionar os campos do formulário que você deseja incluir no e-mail
+    // $dadosFormulario = $request->only(['nome', 'email', 'empresa','qtd','telefone','mensagem']);
 
-    Mail::to('comercial@mental4care.com.br')->send(new NovoEmail($dadosFormulario));
+    // Mail::to('comercial@mental4care.com.br')->send(new NovoEmail($dadosFormulario));
 
-    // Retorne uma resposta adequada após o envio do e-mail, como redirecionar de volta ao formulário
-    return redirect()->back()->with('success', 'E-mail enviado com sucesso!');
+    // // Retorne uma resposta adequada após o envio do e-mail, como redirecionar de volta ao formulário
+    // return redirect()->back()->with('success', 'E-mail enviado com sucesso!');
+
+
+
+
+
+    $destinatarios = [
+        'comercial@mental4care.com.br' => 'Nome Destinatário 1',
+    ];
+    
+    // Validação dos dados do formulário
+    $request->validate([
+        'nome' => 'required|string|max:255',
+        'email' => 'required|email|max:255',
+        'mensagem' => 'required|string',
+    ]);
+
+    // Envio do e-mail
+    $dadosEmail = [
+        'nome' => $request->nome,
+        'email' => $request->email,
+        'empresa' => $request->email,
+        'qtd' => $request->email,
+        'telefone' => $request->email,
+        'mensagem' => $request->mensagem,
+    ];
+
+    foreach ($destinatarios as $email => $nome) {
+        Mail::to($email, $nome)->send(new NovoEmail($dadosEmail));
+    }
+
+
+    return redirect()->back()->with('mensagem', 'E-mail enviado com sucesso!');
 }
 }
--- app/Mail/NovoEmail.php
+++ app/Mail/NovoEmail.php
@@ -3,57 +3,25 @@
 namespace App\Mail;
 
 use Illuminate\Bus\Queueable;
-use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Mail\Mailable;
-use Illuminate\Mail\Mailables\Content;
-use Illuminate\Mail\Mailables\Envelope;
 use Illuminate\Queue\SerializesModels;
 
 class NovoEmail extends Mailable
 {
     use Queueable, SerializesModels;
 
-    /**
-     * Create a new message instance.
-     *
-     * @return void
-     */
-    public function __construct()
+    public $dados;
+
+    public function __construct($dados)
     {
-        //
+        $this->dados = $dados;
     }
 
-    /**
-     * Get the message envelope.
-     *
-     * @return \Illuminate\Mail\Mailables\Envelope
-     */
-    public function envelope()
+    public function build()
     {
-        return new Envelope(
-            subject: 'Novo Email',
-        );
-    }
-
-    /**
-     * Get the message content definition.
-     *
-     * @return \Illuminate\Mail\Mailables\Content
-     */
-    public function content()
-    {
-        return new Content(
-            view: 'nosso-programa',
-        );
-    }
-
-    /**
-     * Get the attachments for the message.
-     *
-     * @return array
-     */
-    public function attachments()
-    {
-        return [];
+        return $this->from($this->dados['email'])
+                    ->subject('Nova mensagem do formulário de contato')
+                    ->view('nosso-programa')
+                    ->with(['dados' => $this->dados]);
     }
 }
